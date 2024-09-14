ddfef37217f5448b8a5282bf4b7bdae63b69ffc2
kevinharv <kharvey2002@gmail.com>
2024-09-13 22:03:07 -0500

    added example from docs

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kevinharv/smtp-relay
 
 go 1.23.1
+
+require (
+	github.com/emersion/go-sasl v0.0.0-20200509203442-7bfe0ed36a21 // indirect
+	github.com/emersion/go-smtp v0.21.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,69 @@
 package main
 
 import (
+	"errors"
+	"io"
 	"log"
 	"os"
+	"time"
+
+	"github.com/emersion/go-sasl"
+	"github.com/emersion/go-smtp"
 )
+
+type Backend struct{}
+
+// NewSession is called after client greeting (EHLO, HELO).
+func (bkd *Backend) NewSession(c *smtp.Conn) (smtp.Session, error) {
+	return &Session{}, nil
+}
+
+// A Session is returned after successful login.
+type Session struct{}
+
+// AuthMechanisms returns a slice of available auth mechanisms; only PLAIN is
+// supported in this example.
+func (s *Session) AuthMechanisms() []string {
+	return []string{sasl.Plain}
+}
+
+// Auth is the handler for supported authenticators.
+func (s *Session) Auth(mech string) (sasl.Server, error) {
+	return sasl.NewPlainServer(func(identity, username, password string) error {
+		if username != "username" || password != "password" {
+			return errors.New("invalid username or password")
+		}
+		return nil
+	}), nil
+}
+
+func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
+	log.Println("Mail from:", from)
+	return nil
+}
+
+func (s *Session) Rcpt(to string, opts *smtp.RcptOptions) error {
+	log.Println("Rcpt to:", to)	
+	return nil
+}
+
+func (s *Session) Data(r io.Reader) error {
+	if b, err := io.ReadAll(r); err != nil {
+		return err
+	} else {
+		log.Println("Data:", string(b))
+	}
+	return nil
+}
+
+func (s *Session) Reset() {}
+
+func (s *Session) Logout() error {
+	return nil
+}
 
 func main() {
 	l := log.New(os.Stdout, "[MAIN]", log.LstdFlags)
-
 
 	discordWebhookURI, present := os.LookupEnv("DISCORD_WEBHOOK")
 	if !present {
@@ -16,12 +72,44 @@ func main() {
 
 	l.Printf("Discord Webhook URI: %s", discordWebhookURI)
 
-
-	
 	// Get Discord (and whatever else) webhook endpoint from env
 	// Start SMTP server
 	// On SMTP message to provider@domain.com
-		// Check provider against configured list
-		// Get email subject, body, sender
-		// Construct webhook message with sender + subject as title, body as body
+	// Check provider against configured list
+	// Get email subject, body, sender
+	// Construct webhook message with sender + subject as title, body as body
+
+	/* EXAMPLE SERVER FROM PACKAGE DOCUMENTATION */
+	// The Backend implements SMTP server methods.
+
+	// ExampleServer runs an example SMTP server.
+	//
+	// It can be tested manually with e.g. netcat:
+	//
+	//	> netcat -C localhost 1025
+	//	EHLO localhost
+	//	AUTH PLAIN
+	//	AHVzZXJuYW1lAHBhc3N3b3Jk
+	//	MAIL FROM:<root@nsa.gov>
+	//	RCPT TO:<root@gchq.gov.uk>
+	//	DATA
+	//	Hey <3
+	//	.
+
+	be := &Backend{}
+
+	s := smtp.NewServer(be)
+
+	s.Addr = "localhost:1025"
+	s.Domain = "localhost"
+	s.WriteTimeout = 10 * time.Second
+	s.ReadTimeout = 10 * time.Second
+	s.MaxMessageBytes = 1024 * 1024
+	s.MaxRecipients = 50
+	s.AllowInsecureAuth = true
+
+	log.Println("Starting server at", s.Addr)
+	if err := s.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
